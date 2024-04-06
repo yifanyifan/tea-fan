8e129eb136fbd16f47fa637ae33f4f90e6d5e485
yifanyifan <895115828@qq.com>
2024-04-06 22:56:44 +0800

    yf

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'tea-cll': 'path/to/tea-cll'
     }
   }
 })
